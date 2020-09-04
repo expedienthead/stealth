5d5f6e8e5419606734b426c00129e5b769cfcc3f
Bao Quoc <quoc@mmj.vn>
2020-09-04 09:18:31 +0700

    Add thread_id to Stealth

--- lib/stealth/controller/controller.rb
+++ lib/stealth/controller/controller.rb
@@ -75,7 +75,10 @@ module Stealth
           flow_controller.send(@action_name)
           run_catch_all(reason: 'Did not send replies, update session, or step') unless flow_controller.progressed?
         rescue StandardError => e
-          Stealth::Logger.l(topic: "catch_all", message: e.backtrace.join("\n"))
+          Stealth::Logger.l(
+            topic: "catch_all",
+            message: [e.message, e.backtrace.join("\n")].join("\n")
+          )
           run_catch_all(reason: e.message)
         end
       end
