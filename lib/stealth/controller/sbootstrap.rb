e1c7c6e49fdef98bda32ff27e1b3eac106061c3d
Bao Quoc <quoc@mmj.vn>
2020-09-21 17:07:58 +0700

    Add debugger

--- lib/stealth/controller/sbootstrap.rb
+++ lib/stealth/controller/sbootstrap.rb
@@ -3,19 +3,18 @@ module Stealth
     module Sbootstrap
       extend ActiveSupport::Concern
 
-      included do
-        def load_default_setup
-          # service_client = Kernel.const_get("Stealth::Services::#{current_service.classify}::Client")
-          # reply_handler = Kernel.const_get("Stealth::Services::#{current_service.classify}::ReplyHandler")
-          reply_handler = reply_handler.new
-          # reply = reply_handler.messenger_profile
-          # client = service_client.new(
-          #   reply: reply,
-          #   endpoint: 'messenger_profile',
-          #   access_token: current_page_info[:access_token]
-          # )
-          # client.transmit
-        end
+      # included do
+      def load_default_setup
+        # service_client = Kernel.const_get("Stealth::Services::#{current_service.classify}::Client")
+        # reply_handler = Kernel.const_get("Stealth::Services::#{current_service.classify}::ReplyHandler")
+        reply_handler = reply_handler.new
+        # reply = reply_handler.messenger_profile
+        # client = service_client.new(
+        #   reply: reply,
+        #   endpoint: 'messenger_profile',
+        #   access_token: current_page_info[:access_token]
+        # )
+        # client.transmit
       end
     end
   end
