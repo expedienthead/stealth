ec57ac7b5731d94af94e57006b4ae38d5f05efd3
Bao Quoc <quoc@mmj.vn>
2020-09-21 16:17:59 +0700

    Add debugger

--- lib/stealth/controller/bootstrap.rb
+++ lib/stealth/controller/bootstrap.rb
@@ -4,31 +4,30 @@ module Stealth
       extend ActiveSupport::Concern
 
       included do
-
         def load_default_setup
           reply_handler = reply_handler.new
           reply = reply_handler.messenger_profile
-          # client = service_client.new(
-          #   reply: reply,
-          #   endpoint: 'messenger_profile',
-          #   access_token: current_page_info[:access_token]
-          # )
-          # client.transmit
+          client = service_client.new(
+            reply: reply,
+            endpoint: 'messenger_profile',
+            access_token: current_page_info[:access_token]
+          )
+          client.transmit
         end
+      end
 
-        private
+      private
 
-        def service_client
-          Kernel.const_get("Stealth::Services::#{current_service.classify}::Client")
-        rescue NameError
-          raise(Stealth::Errors::ServiceNotRecognized, "The service '#{current_service}' was not regconized")
-        end
+      def service_client
+        Kernel.const_get("Stealth::Services::#{current_service.classify}::Client")
+      rescue NameError
+        raise(Stealth::Errors::ServiceNotRecognized, "The service '#{current_service}' was not regconized")
+      end
 
-        def reply_handler
-          Kernel.const_get("Stealth::Services::#{current_service.classify}::ReplyHandler")
-        rescue NameError
-          raise(Stealth::Errors::ServiceNotRecognized, "The service '#{current_service}' was not regconized")
-        end
+      def reply_handler
+        Kernel.const_get("Stealth::Services::#{current_service.classify}::ReplyHandler")
+      rescue NameError
+        raise(Stealth::Errors::ServiceNotRecognized, "The service '#{current_service}' was not regconized")
       end
     end
   end
